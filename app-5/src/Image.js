17984a1375de5aa7a8938b45a5b66265603e576a
Tanner Christensen <tannerchristensen20@gmail.com>
2020-10-22 15:12:00 -0600

    changes

---
+++ app-5/src/Image.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+export default class Image extends Component {
+  render() {
+    return (
+      <div>
+        <img src="https://github.com/DevMountain/react-drills/blob/assets/5.png" />
+        <div>Error 599</div>
+      </div>
+    )
+  }
+}
+```
+export default Image
